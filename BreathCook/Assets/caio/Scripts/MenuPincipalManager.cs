9ac6cbc30b1cdd32b49b9d79dae350cf190e3de1
CaioLucas355 <167205982+CaioLucas355@users.noreply.github.com>
2024-08-16 11:48:46 -0300

    MenuAndDialogDevelopment

---
+++ BreathCook/Assets/caio/Scripts/MenuPincipalManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOptiones;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void AbrirOptions()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOptiones.SetActive(true);
+    }
+    public void FecharOptiones()
+    {
+        painelOptiones.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+}
