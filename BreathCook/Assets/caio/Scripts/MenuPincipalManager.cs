e750d780eb14d6c2c0ca6e92dd988f7500a34d6c
CaioLucas355 <167205982+CaioLucas355@users.noreply.github.com>
2024-08-21 12:17:20 -0300

    MENUSETTINGS

--- BreathCook/Assets/caio/Scripts/MenuPincipalManager.cs
+++ BreathCook/Assets/caio/Scripts/MenuPincipalManager.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPincipalManager : MonoBehaviour
 {
+    public static MenuPincipalManager Instance;
     [SerializeField] private string nomeDoLevelDeJogo;
     [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelOptiones;
+    [SerializeField] private GameObject painelCreditos;
 
+    public Slider musicSlider;
+    private void Awake()
+    {
+        Instance = this;
+        musicSlider.value = PlayerPrefs.GetFloat("Volume");
+    }
     public void Jogar()
     {
         SceneManager.LoadScene(nomeDoLevelDeJogo);
@@ -18,15 +27,36 @@ public class MenuPincipalManager : MonoBehaviour
     {
         painelMenuInicial.SetActive(false);
         painelOptiones.SetActive(true);
+        painelCreditos.SetActive(false);
     }
     public void FecharOptiones()
     {
         painelOptiones.SetActive(false);
         painelMenuInicial.SetActive(true);
+        painelCreditos.SetActive(false);
+    }
+    
+    public void CreditosGame()
+    {
+        painelCreditos.SetActive(true);
+        painelMenuInicial.SetActive(false);
+        painelOptiones.SetActive(false);
+    }
+    public void FecharCreditos()
+    {
+        painelOptiones.SetActive(false);
+        painelMenuInicial.SetActive(true);
+        painelCreditos.SetActive(false);
     }
     public void SairJogo()
     {
         Debug.Log("Sair do Jogo");
         Application.Quit();
     }
+
+    public void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("Volume", musicSlider.value);
+    }
+
 }
