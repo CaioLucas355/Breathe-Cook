efd3e5a6c98019965973049c2d7be03915be205a
Emanuellamonica <167205977+Emanuellamonica@users.noreply.github.com>
2024-09-03 08:00:16 -0300

    transicao

---
+++ BreathCook/Assets/Emanuel/Transicao.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Letreiro : MonoBehaviour
+{
+    [Header("LETREIRO")]
+    float speed = 10f;
+    float textPosBegin = -1366.65f;
+    float boundaryTextEnd = 1366.65f;
+
+    public RectTransform myGorectTransform;
+
+    [SerializeField]
+    TextMeshProUGUI mainText;
+
+    [SerializeField]
+    bool isLooping = false;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+}
